c2f405bc25acd27e87ebb3fd33f66145987c24b7
PKShadow <103926586+ElectricGhost16@users.noreply.github.com>
2026-02-24 12:41:20 +0100

    Initial comit
    Test for the esp32 webserver

---
+++ Programming/webserver_esp32/webserver_esp32.ino
@@ -0,0 +1,61 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+/* Put your SSID & Password */
+const char* ssid = "ESP32";         // Enter SSID here
+const char* password = "12345678";  // Enter Password here
+
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+WebServer server(80);
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handle_OnConnect() {
+  counter++;
+  server.send(200, "text/html", createHTML());
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+String createHTML() {
+  String str = "<!DOCTYPE html> <html>";
+  str += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">";
+  str += "<style>";
+  str += "body {font-family: Arial, sans-serif; color: #444; text-align: center;}";
+  str += ".title {font-size: 30px; font-weight: bold; letter-spacing: 2px; margin: 80px 0 55px;}";
+  str += ".counter {font-size: 80px; font-weight: 300; line-height: 1; margin: 0px; color: #4285f4;}";
+  str += "</style>";
+  str += "</head>";
+  str += "<body>";
+  str += "<h1 class=\"title\">VISITOR COUNTER</h1>";
+  str += "<div class=\"counter\">";
+  str += counter;
+  str += "</div>";
+  str += "</body>";
+  str += "</html>";
+  return str;
+}
